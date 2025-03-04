69b5cf73acdc4bd0cf62eb10cb0f59d31f29c2cc
J0elCar01 <163184521+J0elCar01@users.noreply.github.com>
2025-03-04 14:20:49 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-  $("li a").css("color","white");
-  $(".col-sm-8 ul li a i").css("font-size", "3rem");
-    $(".col-sm-8 ul li a i").css("padding", "0 3rem");
-  $(".card").hover(function(){
-    $(this).toggleClass("animated bounce");
-    $(this).css("cursor","pointer")
-  });
-});
+const body = document.body;
+const content = document.querySelector('.js-content');
+const blocks = document.querySelectorAll('.block');
 
+const updateOffset = () => {
+  requestAnimationFrame(updateOffset);
+  body.style.setProperty('--y', content.scrollTop);
+  updateProps();
+}
+
+const updateProps = () => {  
+  let i = -1;
+  for(let block of blocks) {
+    i += 1;
+    let top = blocks[i].getBoundingClientRect().top;
+    if(top < window.innerHeight*1.3 && top > window.innerHeight*-1.3) {
+      body.style.setProperty(`--yBlock-${i+1}`, top);
+    } else {
+      body.style.setProperty(`--yBlock-${i+1}`, 0); 
+    }
+  }
+}
+
+updateProps();
+updateOffset();
